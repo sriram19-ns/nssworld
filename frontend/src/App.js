efa42c6f064f2735e0220800ba1c450f6d04fca4
sriram19-ns <nssriram19@gmail.com>
2024-07-27 00:16:26 +0530

    Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
-import React from 'react'
+// import React from 'react'
+import {Container} from 'react-bootstrap'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <h1>Welcome to NSSWorld</h1>
+    <>
+    <Header>
+      <main className='py-3'>
+        <Container>
+        <h1>Welcome to NSSWorld</h1>
+        </Container>
+      </main>
+    </Header>
+    </>
   )
 }
 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,26 @@
+// import React from 'react';
+import {Navbar, Nav, Container } from 'react-bootstrap';
+import {FaShoppingCart, FaUser } from 'react-icons/fa';
+
+const Header = () => {
+  return (
+    <Header>
+        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+            <Container>
+                <Navbar.Brand href="/">NSSWorld</Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    <Nav className='ms-auto'>
+                        <Nav.Link href='/cart'><FaShoppingCart/>Cart</Nav.Link>
+                        <Nav.Link href='/login'><FaUser/>Sign in</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+
+            </Container>
+        
+        </Navbar>
+    </Header>
+  )
+}
+
+export default Header
